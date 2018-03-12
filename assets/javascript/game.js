c2e95652b529cba5edc465bae6a5a283a4851aa6
Julian Sandersius <30510599+JSandersius@users.noreply.github.com>
2018-03-11 21:13:25 -0700

    fixing

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+
+function myFunction() {
+	var comChoice; 
+	var wins = 0
+	var losss = 0
+	var chancess = 10; decreas this by one with each 
+
+	var choices = [ ]
+
+	var computerchoice = " "; 
+} 
+
+
+
+
+declared variables 
+create random numbers to grab aphabet 
+
+var userGuess  = 
+
+
+
+declared variables 
+create random numbers to grab aphabet 
+
+var 
+
+
+
+var str = "abcdefghijklmnopqrstuvwxyz";
+var res = str.charAt(Math.Random(Math.floor()*12)+1);  
+
+
