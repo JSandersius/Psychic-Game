4da20733c64357bb32fabec2947d9bc123275222
Julian Sandersius <30510599+JSandersius@users.noreply.github.com>
2018-03-12 20:18:13 -0700

    refactoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,134 +1,42 @@
 // Computer Randomly Picks a letter, user has to guess it.
 // Need variables for wins, losses, chances left, computer choice, choices
 
+var win = 0;
+var loss = 0;
+var guessesLeft = 10;
+var guessTotal = "";
 
+// creating an array with alphabet
+var computerChoices = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-
-// 
-var letters = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var guessedLetters = [];
-var comChoice = null;
-var guessesLeft = 10; // decrease this by one each wrong guess, if reach 0, restart game and add 1 to losses
-
-var wins = 0;
-var losses = 0;
-
-
-//update guesses so far 
-var updateGuessesLeft = function() {
-    document.querySelector("#guessesLeft").innerHTML = guessedLeft;
-};
-
-var updateLetterToGuess = function() {
-    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
-};
-
-var updateGuessesSoFar = functio() {
-    document.querySelector("#querySelector").innrerHTML = guessedLetters.join(",");
-};
-
-
-var reset = function() {
-    guessesLeft = 9;
-    guessesLetters = [];
-    updateLetterToGuess();
-    updateGuessesLeft();
-    updateGuessesSoFar();
-};
-
-updateLetterToGuess();
-updateGuessesLeft();
-
+//runs at a key press 
 document.onkeyup = function(event) {
-    guessesLeft--;
 
-    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    //computers guess, randomly chooses a number
+    var computerGuess = computerChoices.charAt(Math.floor(Math.random() * compututerChoices.length));
+    console.log(computerGuess);
 
-    guessedLetters.push(letter);
+    //Determines which key was pressed. 
+    var userGuess = event.key;
+    totalGuess = totalGuess + "\'" + userGuess + "\'  ";
+    document.getElementById('lbl4').innerHTML = totalGuess;
 
-    updateGuessesLeft();
-    updateGuessesSoFar();
 
-    if (letter === letterToGuess) {
+    if (userGuess === computerGuess) {
+        win++;
+        document.getElementById('lbl1').innerHTML = win;
+        totalGuess = "";
+        document.getElementById('lbl3').innerHTML = guessesLeft;
+    } else {
+        guessesLeft--;
+        document.getElementById('lbl3').innerHTML = guessesLeft;
 
-        wins++;
-        document.querySelector("#wins").innerHTML = wins;
+        if (guessesLeft === 0) {
 
-        reset();
-    }
-
-    if (guessesLeft === 0) {
-
-        lossess++;
-        document.querySelector("#losses").innerHTML = losses;
-
-        reset();
+            loss++;
+            document.getElementById('lbl2').innerHTML = loss;
+            totalGuess = "";
+            guessesLeft = 10;
+        }
     }
 };
-
-
-//updateGuesses
-//updateGuessesLeft
-//updateGuessesSoFar
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function myFunction() {
-// 	var comChoice; 
-// 	var wins = 0
-// 	var losss = 0
-// 	var chancess = 10; decreas this by one with each 
-
-// 	var choices = [ ]
-
-// 	var computerchoice = " "; 
-// } 
-
-
-
-
-// declared variables 
-// create random numbers to grab aphabet 
-
-// var userGuess  = 
-
-
-
-// declared variables 
-// create random numbers to grab aphabet 
-
-// var 
-
-
-
-// var str = "abcdefghijklmnopqrstuvwxyz";
-// var res = str.charAt(Math.Random(Math.floor()*12)+1);
